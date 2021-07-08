f7904148e23458812e2840e902def677015d9eab
Clesio Freitas <clesio.fb@hotmail.com>
2021-07-07 23:29:45 -0300

    tabnav transformado em classe

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,21 +1,37 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+export default  class TabNav {
+  constructor ( menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+   this.tabContent = document.querySelectorAll(content);
+   this.activeClass = 'ativo'
+  }
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
+  // ativa a tab de acordoo  com  o index da mesma 
+   activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
     const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+   this.tabContent[index].classList.add(this.activeClass, direcao);
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
+  //aciciona os eventos nas tabs
+  addTabNavEvent ( ) {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', ( ) => this.activeTab (index));
         activeTab(index);
-      });
-    });
-  }
+   });
 }
+
+  init ( ) {
+    if  (this.tabMenu.length && this.tabContent.length) {
+      // ativar primerio item
+      this.activeTab(0);
+      this.addTabNavEvent ( ) ;
+      }
+    }
+}
+
+  if  {
+    tabContent[0].classList.add(this.activeClass);
+  }
+
